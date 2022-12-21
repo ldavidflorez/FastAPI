7b65ab5b2e648a920f46e92ea1c837dd6fc85ca0
Luis F <luisflorexpareja@gmail.com>
2022-12-20 21:57:52 -0500

    Book API in local storage

--- books.py
+++ books.py
@@ -1,23 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI
-from enum import Enum
 
 app = FastAPI()
 
-
-class DirectionName(str, Enum):
-    north = "north"
-    south = "south"
-    east = "east"
-    west = "west"
-
-
 BOOKS = {
-    'book_1': {'title': 'Title One', 'author': 'Author One'},
-    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
-    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
-    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
-    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title Two", "author": "Author Two"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"},
 }
 
 
@@ -35,13 +26,35 @@ async def read_one_book(book_name: str):
     return BOOKS[book_name]
 
 
-@app.get("/directions/{direction_name}")
-async def get_durection(direction_name: DirectionName):
-    if direction_name == DirectionName.north:
-        return {"direction": direction_name}
-    elif direction_name == DirectionName.south:
-        return {"direction": direction_name}
-    elif direction_name == DirectionName.east:
-        return {"direction": direction_name}
-    else:
-        return {"direction": direction_name}
+@app.get("/assigment/")
+async def read_one_book(book_name: str):
+    return BOOKS[book_name]
+
+
+@app.post("/")
+async def create_book(book_title: str, book_author: str):
+    last_book_id = max([int(book.split("_")[-1]) for book in BOOKS])
+    BOOKS[f"book_{last_book_id + 1}"] = {"title": book_title,
+                                         "author": book_author}
+    return BOOKS[f"book_{last_book_id + 1}"]
+
+
+@app.put("/{book_name}")
+async def update_book(book_name: str, book_title: Optional[str] = None, book_author: Optional[str] = None):
+    if book_title:
+        BOOKS[book_name]["title"] = book_title
+    if book_author:
+        BOOKS[book_name]["author"] = book_author
+    return BOOKS[book_name]
+
+
+@app.delete("/{book_name}")
+async def delete_book(book_name: str):
+    del BOOKS[book_name]
+    return {"message": f"{book_name} was deleted"}
+
+
+@app.delete("/assigment/")
+async def delete_book(book_name: str):
+    del BOOKS[book_name]
+    return {"message": f"{book_name} was deleted"}
