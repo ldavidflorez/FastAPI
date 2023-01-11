9fbeb2a351677d0abd5d79233eea893e1cab1850
Luis F <luisflorexpareja@gmail.com>
2023-01-10 22:05:11 -0500

    sqlite complete CRUD - FastAPI

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,12 +1,94 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Todo not found")
+
+
+def sucessfull_response(status_code: int):
+    return {"status": status_code, "transaction": "sucessfull"}
+
+
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(
+        gt=0, lt=6, description="The priority must be between 1-5")
+    complete: bool
+
+
 @app.get("/")
-async def create_database():
-    return {"message": "database was created"}
+async def read_todos(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(
+        models.Todos.id == todo_id).first()
+    if todo_model:
+        return todo_model
+    raise http_exception()
+
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return sucessfull_response(201)
+
+
+@app.put("/todo/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(
+        models.Todos.id == todo_id).first()
+    if todo_model:
+        todo_model.title = todo.title
+        todo_model.description = todo.description
+        todo_model.priority = todo.priority
+        todo_model.complete = todo.complete
+
+        db.add(todo_model)
+        db.commit()
+
+        return sucessfull_response(200)
+
+    raise http_exception()
+
+
+@app.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(
+        models.Todos.id == todo_id).first()
+    if todo_model:
+        db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+
+        db.commit()
+
+        return sucessfull_response(200)
+
+    raise http_exception()
