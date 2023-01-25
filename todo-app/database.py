99128a8c8ca2a4e05d6d915623d03e906837c3eb
Luis F <luisflorexpareja@gmail.com>
2023-01-25 14:15:48 -0500

    Adding PostgreSQL database

--- todo-app/database.py
+++ todo-app/database.py
@@ -2,10 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# for SQLite3
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+#                        "check_same_thread": False})
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+
+# for PostgreSQL
+
+SQLALCHEMY_DATABASE_URL = "postgresql://luisf:pass123@localhost:5432/tododb"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
